cbbe438f22a9f38ac7a4d2270312de6ba32d1c35
Sweet_Scientist <64701309+AdonisAgelis@users.noreply.github.com>
2021-12-07 19:47:44 +0200

    Refreshing UI accordingly

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import mutation from '../mutations/Login';
+import query from '../queries/CurrentUser';
 
 import AuthForm from './AuthForm';
 
@@ -8,6 +9,7 @@ class LoginForm extends Component {
   onSubmit({ email, password }) {
     this.props.mutate({
       variables: { email, password },
+      refetchQueries: [{ query }],
     });
   }
 
