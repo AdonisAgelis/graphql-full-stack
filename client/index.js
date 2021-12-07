767f0c49cd0051733ef974da4323f4a1d0c80d2b
Sweet_Scientist <64701309+AdonisAgelis@users.noreply.github.com>
2021-12-07 22:24:53 +0200

    SignUp Mutation

--- client/index.js
+++ client/index.js
@@ -7,6 +7,7 @@ import { ApolloProvider } from 'react-apollo';
 
 import App from './components/App';
 import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
 
 const networkInterface = createNetworkInterface({
   uri: '/graphql',
@@ -26,6 +27,7 @@ const Root = () => {
       <Router history={hashHistory}>
         <Route path="/" component={App}>
           <Route path="/login" component={LoginForm} />
+          <Route path="/signup" component={SignupForm} />
         </Route>
       </Router>
     </ApolloProvider>
