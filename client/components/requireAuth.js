f98548ab093a81397a0ff9e98b9cbf00000a5c96
Sweet_Scientist <64701309+AdonisAgelis@users.noreply.github.com>
2021-12-08 04:15:34 +0200

    Final Commit

--- client/components/requireAuth.js
+++ client/components/requireAuth.js
@@ -3,12 +3,18 @@ import { hashHistory } from 'react-router';
 import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
 
-class RequireAuth extends Component {
-  componentDidMount() {
-    if (!this.props.data.loading && !this.props.data.user) {
-      hashHistory.push('/login');
+export default WrappedComponent => {
+  class RequireAuth extends Component {
+    componentWillUpdate(nextProps) {
+      if (!nextProps.data.loading && !nextProps.data.user) {
+        hashHistory.push('/login');
+      }
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />;
     }
   }
-}
 
-graphql(query)(RequireAuth);
+  return graphql(query)(RequireAuth);
+};
