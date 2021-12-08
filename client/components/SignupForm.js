f8ecc02672c6feedabfaf3c03b0d31928b1140fb
Sweet_Scientist <64701309+AdonisAgelis@users.noreply.github.com>
2021-12-08 03:00:03 +0200

    Handling more errors

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import mutation from '../mutations/Signup';
+import query from '../queries/CurrentUser';
 
 import AuthForm from './AuthForm';
 
 class SignupForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { errors: [] };
+  }
+
   onSubmit({ email, password }) {
-    this.props.mutate({
-      variables: { email, password },
-    });
+    this.props
+      .mutate({
+        variables: { email, password },
+        refetchQueries: [{ query }],
+      })
+      .catch(error => {
+        const errors = error.graphQLErrors.map(err => err.message);
+        this.setState({ errors });
+      });
   }
 
   render() {
     return (
       <div>
         <h3>Sign Up</h3>
-        <AuthForm errors={[]} onSubmit={this.onSubmit.bind(this)} />
+        <AuthForm
+          errors={this.state.errors}
+          onSubmit={this.onSubmit.bind(this)}
+        />
       </div>
     );
   }
